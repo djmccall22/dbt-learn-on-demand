18e036489a634764dee87da00d839fc9d413582e
djmccall11@gmail.com <djmccall11@gmail.com>
2021-10-09 00:45:15 +0000

    updated tests, docs, and sources

---
+++ models/staging/raw/stg_payments.sql
@@ -0,0 +1,17 @@
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod,
+        status,
+        amount / 100 as amount,
+        created,	
+        _batched_at
+
+    from {{source('raw','payments')}}
+
+)
+
+select * from payments
+
